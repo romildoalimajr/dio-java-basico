0476c7f8f1be93bf57da41c843f5ab39ba9ab928
Romildo A. Lima Jr <odlimor.sevla@gmail.com>
2024-08-08 09:31:30 -0300

    Anatomia das Classes - 4

--- java-anatomia-classes/src/MyClass.java
+++ java-anatomia-classes/src/MyClass.java
@@ -1,24 +1,6 @@
 public class MyClass {
     public static void main(String[] args){
 
-        String myName = "Romildo";
-
-        int anoFabricacao = 2022;
-
-        boolean verdadeira = true;
-        boolean falsa = false;
-
-        anoFabricacao = 2018;
-
-        String primeiroNome = "Romildo";
-        String segundoNome = "Alves";
-
-        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
-
-        System.out.println(nomeCompleto);
-    }
-
-    public static String nomeCompleto(String primeiroNome, String segundoNome){
-        return "Resultado do método.: " + primeiroNome.concat(" ").concat(segundoNome);
+       
     }
 }
